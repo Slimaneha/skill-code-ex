c0d66b822f14787bb1c3f779364976eb9fd4d00e
root <root@ahah.lan>
2017-07-19 15:10:49 +0200

    Ex calcule ages Fini

---
+++ Calculer mon âge/js/main.js
@@ -0,0 +1,17 @@
+
+// Réqupération de l'ages en chaine de caractaire 
+
+var aneesAc = prompt ("En quelle année somme nous ?" )  
+var aneesN = prompt ("Quelles est votre anneé de naissances")
+
+
+// On convertie la chaine de caractaire en chiffre 
+
+var ag=parseInt(aneesAc);
+var ag2=parseInt(aneesN);
+
+
+
+// et on Fais le Calcule  
+
+alert(ag - ag2 ) ;
