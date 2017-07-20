bfbc7000952ed854239b65aed1209b9c7bd97da9
root <root@ahah.lan>
2017-07-20 09:10:47 +0200

    ex ages fini

---
+++ ex 1 pages 3/js/main.js
@@ -0,0 +1,37 @@
+
+var ages = prompt("Quelle ages avais vous ?" ) ;
+
+
+
+
+
+
+
+
+if (ages >= 21 ){
+    alert ("Vous pouvais rendtré sur le site")
+}
+
+else {
+    alert ("Vous avais pas le droit d'allez sur le site ")
+}
+
+
+if (ages%2 == 0)
+   alert ("Votre ages et un chiffre paire")
+else
+       alert ("Votre ages et un chiffre inpaire")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
