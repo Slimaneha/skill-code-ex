43773c0da6b91ea7a50d37fac0b485b3b657ab5b
root <root@ahah.lan>
2017-07-24 09:34:22 +0200

    Ajouts de l'excercice l'échéque

---
+++ ex piramide/html5-boilerplate_v5.3.0/js/main.js
@@ -0,0 +1,22 @@
+
+
+var etoile ='';
+
+
+for (var i =1 ; i<=5; i++) {
+
+    for (var j=1; j<=i;j++) {
+
+        etoile+= "*";
+      
+    }
+    etoile += '\n';
+    //
+
+}
+console.log(etoile);
+
+
+
+
+                    
---
+++ html5-boilerplate_v5.3.0-Bosstrap-4.0.0/html5-boilerplate_v5.3.0/js/main.js
@@ -0,0 +1,17 @@
+var etoile ='';
+
+
+for (var i =8 ; i<=9; i++) {
+
+    for (var j=1; j<=i;j++) {
+
+        etoile+= "*";
+      
+    }
+    etoile +="\n"
+    // etoile += '\n';
+    //
+
+}
+console.log(etoile);
+
